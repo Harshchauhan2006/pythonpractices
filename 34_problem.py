c0c7f6958a2d29e171aab60eed00d0cfa00fd346
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-17 21:34:48 +0530

    Update 34_problem.py

--- 34_problem.py
+++ 34_problem.py
@@ -1,1 +1,10 @@
-#Write a program to find out weather the given post is talking about is talking about harsh or not 
+#Write a program to find out weather the given post is talking about is talking about harsh or not.
+post=input("enter post :-")
+
+if("Harsh"  in post):
+    print("this post is talking about Harsh")
+
+else:
+    print("this post not  talk about Harsh")
+
+
