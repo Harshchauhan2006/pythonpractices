4a662122bdf1c0ed015250b5ecb23747f49820c9
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-17 21:54:22 +0530

    Create 35_problem.py

---
+++ 35_problem.py
@@ -0,0 +1,6 @@
+#write a to print multipication table of a given number using for loop.
+num=int(input("Enter a number to print its multiplication table: "))
+number=num
+for i in range(1,11):
+    print(f"{"num"}*{i} = {num*i}")
+
