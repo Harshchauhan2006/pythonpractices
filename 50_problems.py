5860e9843101aa529735d9401dac44b3bab05fae
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-27 20:44:20 +0530

    Update 50_problems.py

--- 50_problems.py
+++ 50_problems.py
@@ -1,1 +1,11 @@
+#Write a python function to remove a given word from a list ad strip it at the same time.
+def rem(l, word):
+    n=[]
+    for item in l:
+        if not(item == word):
+            n.append(item.strip())
+    return n
 
+l= ['  apple  ', 'banana', '  mango', 'orange  ', 'banana  ']
+
+print(rem(l, 'banana'))
