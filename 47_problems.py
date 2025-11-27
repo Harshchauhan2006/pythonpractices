c5d56ec1454d9d83f03295ee8aa2163963c849b3
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-27 20:11:44 +0530

    Update 47_problems.py

--- 47_problems.py
+++ 47_problems.py
@@ -1,1 +1,11 @@
+#Write a recursive function to calculate the sum of first n natural numbers.
+def sum(n):
+    if n==1:
+        return 1
+    return sum(n-1) +n
+
+n=int(input("enter a number:-"))
+print(sum(n))
+
+
 
